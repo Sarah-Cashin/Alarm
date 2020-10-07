fbef6cbd2299e3b2a6d895ad0715efa36b5709a3
Sarah-Cashin <62601861+Sarah-Cashin@users.noreply.github.com>
2020-10-07 08:52:16 -0700

    Second commit

--- Alarmone/clock.js
+++ Alarmone/clock.js
@@ -4,13 +4,11 @@ function startTime() {
     let today = new Date();
     let h = today.getHours();
     let m = today.getMinutes();
-    let s = today.getSeconds();
     let t = setTimeout(startTime, 500);
     let ampm = today.getHours() < 12 ? "AM" : "PM";
     m = checkTime(m);
-    s = checkTime(s);
 
-    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s + " " + ampm
+    document.getElementById('clock').innerHTML = h + ":" + m + " " + ampm
 
 }
 
--- Alarmone/myalarm.js
+++ Alarmone/myalarm.js
@@ -1,11 +1,27 @@
 //Alarm
+//
 
-//setAlarm.addEventListener("click", setAlarm);
+// trying to get the time and clock to link to change the background colour
 
-// function setAlarm() {
-//     if (document.getElementById("setAlarm").innerText === startTime()) {
-//         alert("Alarm");
+// function changeBackground() {
+//     let time = document.getElementById("alarmtime");
+//     let clock = document.getElementById("clock");
+
+//     if (clock = time) {
+//         document.body.style.backgroundColor = "yellow";
 //     }
 
 // }
-// setAlarm();
+
+// function changeBackground() {
+//     let time = new Date.getHours();
+//     if (time < 17) {
+//         document.body.style.backgroundColor = "yellow";
+//     }
+
+// }
+
+function changeBackground() {
+    let alarm = document.getElementById("alarmtime").alarm__time;
+    document.body.style.backgroundColor = "yellow";
+}
