2b598700d905987fa00ef9358d55cf345f611cab
Sarah-Cashin <62601861+Sarah-Cashin@users.noreply.github.com>
2020-11-23 05:30:01 -0800

    Third commit

--- Alarmone/clock.js
+++ Alarmone/clock.js
@@ -8,13 +8,24 @@ function startTime() {
     let ampm = today.getHours() < 12 ? "AM" : "PM";
     m = checkTime(m);
 
-    document.getElementById('clock').innerHTML = h + ":" + m + " " + ampm
-
+    document.getElementById("clock").innerHTML = h + ":" + m + " " + ampm;
 }
 
 function checkTime(i) {
     if (i < 10) {
-        i = "0" + i
-    };
+        i = "0" + i;
+    }
     return i;
 }
+
+//Alarm
+// function changeBackground() {
+//     let alarm = document.getElementById("alarmtime").alarm__time;
+//     document.body.style.backgroundColor = "yellow";
+// }
+
+document.querySelector(".setAlarm").addEventListener("click", function() {
+    if (oninput === startTime) {
+        document.body.backgroundColor = "blue";
+    }
+});
